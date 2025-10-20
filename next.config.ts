f67235ee76e8999e1ada5d3d6c75839a732e0cf7
johntarus <tarusjohn96@gmail.com>
2025-10-20 14:16:37 +0300

    Removed output

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    output: "standalone",
 };
 
 export default nextConfig;
