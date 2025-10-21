d2dec4d186306053719744123b534584ce99cb6a
johntarus <tarusjohn96@gmail.com>
2025-10-21 10:24:39 +0300

    Updated services submenus to coming soon

---
+++ app/services/tax-filing/page.tsx
@@ -0,0 +1,5 @@
+import ComingSoonPage from "@/app/coming-soon/page";
+
+export default function TaxFilingPage() {
+    return <ComingSoonPage/>
+}
---
+++ app/services/tax-planning/page.tsx
@@ -0,0 +1,5 @@
+import ComingSoonPage from "@/app/coming-soon/page";
+
+export default function TaxPlanningPage() {
+    return <ComingSoonPage/>
+}
